649539d4a54d366b42c67d5c08b8de7c2a4cd543
Rohan Gupta <39908472+DemonDaddy22@users.noreply.github.com>
2018-07-25 19:23:17 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -12,48 +12,66 @@ import numpy as np
 
 lock = threading.Lock()
 
-class CoordinatesThread(threading.Thread):
+class Coordinates:
     coordinates = []
 
     def __init__(self, add):
-        threading.Thread.__init__(self)
         self.add = add
 
-    def run(self):
-        lock.acquire()
+    def findCoordinates(self):
         link = 'http://maps.googleapis.com/maps/api/geocode/json'
         address = self.add
         params = { 'address': address }
         response = requests.get(link, params = params)
-        # time.sleep(1)
+        time.sleep(1)
         data = response.json()
-        loc = data['results'][0]['address_components'][0]['long_name']
-        if address == loc:
-            lat = data['results'][0]['geometry']['location']['lat']
-            CoordinatesThread.coordinates.append(lat)
-            lon = data['results'][0]['geometry']['location']['lng']
-            CoordinatesThread.coordinates.append(lon)
-        else:
-            pass
-        lock.release()
+        lat = data['results'][0]['geometry']['location']['lat']
+        Coordinates.coordinates.append(lat)
+        lon = data['results'][0]['geometry']['location']['lng']
+        Coordinates.coordinates.append(lon)
         
     def getCoordinates(self):
-        return CoordinatesThread.coordinates
+        return Coordinates.coordinates
 
 # This thread will send request to fetch weather conditions of specified city
 
-class CityThread (threading.Thread):
-    weather_params = []
+class City:
+    weather_params = {}
 
-    def __init__(self, loc):
-        threading.Thread.__init__(self)
-        self.loc = loc
+    def __init__(self, coordinates):
+        self.coordinates = coordinates
 
-    def run(self):
-        location = self.loc
-        link = ''
-        
-# Use the city api call
+    def findWeather(self):
+        coor = self.coordinates
+        lat = coor[0]
+        lon = coor[1]
+        user_api = 'f50cf750f8468c9127cf0a7aa1a37409'
+        unit = 'metric'
+        api = 'http://api.openweathermap.org/data/2.5/weather?lat='
+        link = api + str(lat) + '&lon=' + str(lon) + '&mode=json&units=' + unit + '&APPID=' + user_api
+        response = requests.get(link)
+        time.sleep(1)
+        data = response.json()
+        weather = data['weather'][0]['main']
+        temp = data['main']['temp']
+        pressure = data['main']['pressure']
+        humidity = data['main']['humidity']
+        min_temp = data['main']['temp_min']
+        max_temp = data['main']['temp_max']
+        wind_speed = data['wind']['speed']
+        City.weather_params = {'weather': weather, 'temp': temp, 'pressure': pressure, 'humidity': humidity, 'min_temp': min_temp, 'max_temp': max_temp, 'wind_speed': wind_speed}
+    
+    def getWeather(self):
+        return City.weather_params
+
+loc = input('Enter location: ')
+c1 = Coordinates(loc)
+c1.findCoordinates()
+coordinates = c1.getCoordinates()
+c2 = City(coordinates)
+c2.findWeather()
+test = c2.getWeather()
+print (test)
 # Write to DB
 # Check if entered city exists in the world
 # Create city class and separate out all necessary weather conditions
